7734b3984df3a22ac81d4e4326e84b9269ba1a69
Daniel Leal Pimenta <129066504+daniellealpimenta@users.noreply.github.com>
2025-09-25 17:16:18 -0300

    Adding Project using package

---
+++ Challenge08App/Challenge08App/Challenge08AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Challenge08AppApp.swift
+//  Challenge08App
+//
+//  Created by Daniel Leal PImenta on 25/09/25.
+//
+
+import SwiftUI
+
+@main
+struct Challenge08AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Challenge08App/Challenge08App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Challenge08App
+//
+//  Created by Daniel Leal PImenta on 25/09/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
